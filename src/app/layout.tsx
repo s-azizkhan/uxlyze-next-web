bdadc31904827f637b80a8e8adebdca68f1538d1
Aziz Khan <aziz@logicwind.com>
2024-09-04 19:50:43 +0530

    added clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 
 import { Providers } from "../../components/Providers";
 import { APP_NAME } from "@/config/app.config";
+import Script from "next/script";
 
 const bricolage = Bricolage_Grotesque({
   subsets: ["latin", "latin-ext", "vietnamese"],
@@ -23,7 +24,20 @@ export default function RootLayout({
   return (
     <html lang="en" className="scrollbar-none">
       <Providers>
-        <body className={`${bricolage.className}`}>{children}</body>
+        <body className={`${bricolage.className}`}>
+          {process.env.NODE_ENV === 'production' && (
+            <Script id="microsoft-clarity-analytics">
+              {`
+                (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "nxzj4u1l0r");
+              `}
+            </Script>
+          )}
+          {children}
+        </body>
       </Providers>
     </html>
   );
