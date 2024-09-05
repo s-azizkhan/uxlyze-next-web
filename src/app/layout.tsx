4eda5564f51501418b41e4d81f586660b560c65a
Aziz Khan <aziz@logicwind.com>
2024-09-05 14:57:32 +0530

    clarity script update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,21 +23,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scrollbar-none">
-      <Providers>
-        <body className={`${bricolage.className}`}>
-          {process.env.NODE_ENV === 'production' && (
-            <Script id="microsoft-clarity-analytics">
-              {`
+      {process.env.NODE_ENV === "production" && (
+        <Script id="microsoft-clarity-analytics">
+          {`
                 (function(c,l,a,r,i,t,y){
                   c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                   t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                   y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
               })(window, document, "clarity", "script", "nxzj4u1l0r");
               `}
-            </Script>
-          )}
-          {children}
-        </body>
+        </Script>
+      )}
+      <Providers>
+        <body className={`${bricolage.className}`}>{children}</body>
       </Providers>
     </html>
   );
