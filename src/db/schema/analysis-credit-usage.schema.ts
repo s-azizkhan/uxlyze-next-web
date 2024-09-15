5de841d0060778135615e55aeab0d13ef436d7ed
Aziz Khan <aziz@logicwind.com>
2024-09-15 18:05:47 +0530

    remove unused imports

--- src/db/schema/analysis-credit-usage.schema.ts
+++ src/db/schema/analysis-credit-usage.schema.ts
@@ -1,5 +1,5 @@
 // analysis-credit-usage.schema.ts
-import { pgTable, uuid, text, timestamp, integer } from "drizzle-orm/pg-core";
+import { pgTable, uuid, timestamp, integer } from "drizzle-orm/pg-core";
 import { usersTable } from "./user.schema";
 import { relations } from "drizzle-orm";
 import { reportsTable } from "./report.schema";
--- src/db/schema/analysis-credit.schema.ts
+++ src/db/schema/analysis-credit.schema.ts
@@ -1,8 +1,7 @@
 // analysis-credit.schema.ts
-import { pgTable, uuid, text, timestamp, integer } from "drizzle-orm/pg-core";
+import { pgTable, uuid, timestamp, integer } from "drizzle-orm/pg-core";
 import { usersTable } from "./user.schema";
 import { relations } from "drizzle-orm";
-import { reportsTable } from "./report.schema";
 import { analysisCreditUsageTable } from "./analysis-credit-usage.schema";
 export const analysisCreditsTable = pgTable("analysis_credits", {
   id: uuid("id").primaryKey().defaultRandom(),
